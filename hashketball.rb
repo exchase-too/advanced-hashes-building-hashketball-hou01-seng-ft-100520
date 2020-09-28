15cfded618c8a7fb6a2a68d8faac34c840f65c56
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-28 16:52:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,7 +16,7 @@ def game_hash
           :steals => "3",
           :blocks => "1",
           :slam_dunks => "1"
-        }
+        },
         {
           :player_name => "Reggie Evans",
           :number => "30",
@@ -27,7 +27,7 @@ def game_hash
           :steals => "12",
           :blocks => "12",
           :slam_dunks => "7"
-        }
+        },
         {
           :player_name => "Brook Lopez",
           :number => "11",
